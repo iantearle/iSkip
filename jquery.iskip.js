ed404e6335a0f2d3d7a1fa45011e5a7005965544
Ben <ben@goinvo.com>
2012-11-16 09:54:59 -0500

    Added option for a different target than image and updated readme.

--- jquery.iskip.js
+++ jquery.iskip.js
@@ -14,12 +14,17 @@ jQuery.fn.iskip = function(options){
 		options = options || {};
 		options.images = options.images || [];
 		options.method = options.method || "click"
+		options.img = options.img;
 		options.cycle = options.cycle || 1;
 		options.resetMargin = options.resetMargin || 0;
 
     return this.each(function(){
 			var imgArr = [];
+			var target = $(this);
 			var pic = $(this);
+			if(typeof options.img !== 'undefined'){
+				pic = options.img;
+			}
 		// Preload Images
 				$.each(options.images, function(index, record) { var o =$("<img>").attr("src",record);		
 				$("body").append(o); o.hide(); });
@@ -32,35 +37,35 @@ jQuery.fn.iskip = function(options){
 			imgArr.push(options.images[0]);
 
 			if (options.method == "mousemove")
-				pic.mousemove(function(e) {
-					pic.attr("src",imgArr[Math.floor((e.pageX - pic.offset().left) / (pic.width()/imgArr.length))]);
+				target.mousemove(function(e) {
+					pic.attr("src",imgArr[Math.floor((e.pageX - target.offset().left) / (target.width()/imgArr.length))]);
 				});
 
 			if (options.method == "click")
 			{			
 					var follower
 					if (!$.browser.msie)
-					{	follower = $("<div>").css({"z-index":0, "width":"15px", "height":"15px", "position":"absolute", "top": pic.offset().top, "left":pic.offset().left});
-  					$("body").append(follower);
+					{	follower = $("<div>").css({"z-index":0, "width":"15px", "height":"15px", "position":"absolute", "top": target.offset().top, "left":target.offset().left});
+					$("body").append(follower);
 						disableSelection(follower[0]);
 					}
 					disableSelection(pic[0]);
 					var imgSrc, enabled;
-					pic.mousemove(function(e) {
-						if (e.pageX<=pic.offset().left+options.resetMargin || e.pageX > pic.offset().left + pic.width()-options.resetMargin || e.pageY<=pic.offset().top+options.resetMargin || e.pageY>=pic.offset().top+pic.height()-options.resetMargin)
+					target.mousemove(function(e) {
+						if (e.pageX<=target.offset().left+options.resetMargin || e.pageX > target.offset().left + target.width()-options.resetMargin || e.pageY<=target.offset().top+options.resetMargin || e.pageY>=target.offset().top+target.height()-options.resetMargin)
 						{	enabled=false;
 							return false;
 						}
 						if (follower)
 							follower.css({"top": e.pageY-7, "left": e.pageX-7});
 						if (enabled==true)
-							pic.attr("src",imgArr[Math.floor((e.pageX - pic.offset().left) / (pic.width()/imgArr.length))]);
+							pic.attr("src",imgArr[Math.floor((e.pageX - target.offset().left) / (target.width()/imgArr.length))]);
 					})
-					pic.add((follower)?follower:null).mouseup(function() {
+					target.add((follower)?follower:null).mouseup(function() {
 							enabled=false; 
 					}).mousedown(function() { enabled=true; });
 			}
-	});			
+	});
 
 	// Disable text selection
 	function disableSelection(element) {
